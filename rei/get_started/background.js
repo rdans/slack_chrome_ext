1888ad7c25c0708b1ad992a919ce5854be571f7c
Reinaldo Daniswara <reinaldodaniswara@Reinaldo-Entos-MacBook-Pro.local>
2020-12-19 15:10:13 -0800

    try get started chrome ext, next: read layer logic

---
+++ rei/get_started/background.js
@@ -0,0 +1,27 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+// chrome.storage API to store, retrieve, and track changes to user data.
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+      console.log('The color is green.');
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostEquals: 'developer.chrome.com'},
+        })
+        ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+    });
+  });
+
+// most APIs, like storage API need to add at permission at manifest file
+// dont forget to add declarativeContent to the permission as well.
+// declarativeContent.PageStateMatcher -> when a user navigate to developer.chrome.com
+// now the gray icon will turn into colored icon.
